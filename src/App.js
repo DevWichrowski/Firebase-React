5702560d830011765affa7e9a3ed450e1179d0e2
devWichrowski <wichrowski.patryk92@gmail.com>
2019-03-15 21:29:33 +0100

    Added Dashboard, Notification and ProjectList Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import { Navbar } from './components/Layout/Navbar';
+import Dashboard from './components/Dashboard/Dashboard';
 
 class App extends Component {
 	render() {
@@ -9,6 +10,9 @@ class App extends Component {
 			<BrowserRouter>
 				<div className="App">
 					<Navbar />
+					<Switch>
+						<Route path="/" component={Dashboard} />
+					</Switch>
 				</div>
 			</BrowserRouter>
 		);
---
+++ src/components/Projects/ProjectList.js
@@ -0,0 +1,36 @@
+import React from 'react';
+const ProjectList = () => {
+	return (
+		<div>
+			<div className="project-list section">
+				<div className="card z-depth-0 project-summary">
+					<div className="card-content grey-text text-darken-3">
+						<span className="card-title">
+							<p>Posted by devWichrowski</p>
+							<p className="grey-text">3th sept</p>
+						</span>
+					</div>
+				</div>
+
+				<div className="card z-depth-0 project-summary">
+					<div className="card-content grey-text text-darken-3">
+						<span className="card-title">
+							<p>Posted by devWichrowski</p>
+							<p className="grey-text">3th sept</p>
+						</span>
+					</div>
+				</div>
+
+				<div className="card z-depth-0 project-summary">
+					<div className="card-content grey-text text-darken-3">
+						<span className="card-title">
+							<p>Posted by devWichrowski</p>
+							<p className="grey-text">3th sept</p>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default ProjectList;
