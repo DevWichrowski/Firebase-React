6501a0f1c1d33ac016d8e7cd08741a6e0e8960a3
devWichrowski <wichrowski.patryk92@gmail.com>
2019-03-15 22:10:58 +0100

    Add ProjectDetails component with props and id

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.scss';
 import { Navbar } from './components/Layout/Navbar';
 import Dashboard from './components/Dashboard/Dashboard';
+import ProjectDetails from './components/Projects/ProjectDetails';
 
 class App extends Component {
 	render() {
@@ -11,7 +12,8 @@ class App extends Component {
 				<div className="App">
 					<Navbar />
 					<Switch>
-						<Route path="/" component={Dashboard} />
+						<Route exact path="/" component={Dashboard} />
+						<Route path="/project/:id" component={ProjectDetails} />
 					</Switch>
 				</div>
 			</BrowserRouter>
---
+++ src/components/Projects/ProjectDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ProjectDetails = (props) => {
+	const id = props.match.params.id;
+	return (
+		<div className="container section project-details">
+			{console.log(props)}
+			<div className="card z-depth-0">
+				<div className="card-content">
+					<span className="card-title">Project title - {id}</span>
+					<p>Lorem ipsum</p>
+				</div>
+				<div className="card-action gret lighten-4 grey-text">
+					<div>Postem by devWichrowski</div>
+					<div>2nd september, 2am</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProjectDetails;
