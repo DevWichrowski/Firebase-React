9d44dd5410505691b8ecbc5254d712bf73e490db
devWichrowski <wichrowski.patryk92@gmail.com>
2019-04-17 18:36:26 +0200

    Connected redux to firestore
    17

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,15 @@ import 'firebase/auth';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCbiYv8WV2JNqSMk6hgWusEVb1PdERXulw",
-    authDomain: "fir-react-app-826cd.firebaseapp.com",
-    databaseURL: "https://fir-react-app-826cd.firebaseio.com",
-    projectId: "fir-react-app-826cd",
-    storageBucket: "fir-react-app-826cd.appspot.com",
-    messagingSenderId: "221082949318"
-  };
+	apiKey: 'AIzaSyCbiYv8WV2JNqSMk6hgWusEVb1PdERXulw',
+	authDomain: 'fir-react-app-826cd.firebaseapp.com',
+	databaseURL: 'https://fir-react-app-826cd.firebaseio.com',
+	projectId: 'fir-react-app-826cd',
+	storageBucket: 'fir-react-app-826cd.appspot.com',
+	messagingSenderId: '221082949318'
+};
 
-  firebase.initializeApp(config);
-  firebase.firestore().settings({timestampsInSnapshots: true })
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
-const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+	rootReducer,
+	{},
+	compose(
+		composeWithDevTools(
+			applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+			reduxFirestore(fbConfig),
+			reactReduxFirebase(fbConfig)
+		)
+	)
+);
 
 ReactDOM.render(
 	<Provider store={store}>
