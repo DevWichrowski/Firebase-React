6461067470f7a0bd6009973f986b66c085254e43
devWichrowski <wichrowski.patryk92@gmail.com>
2019-04-19 22:10:12 +0200

    Rendering loggedin links or loggedout
    25

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -4,7 +4,10 @@ import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+	const { auth } = props;
+
+	console.log(auth)
 	return (
 		<div className="Navbar">
 			<nav className="nav-wrapper grey darken-3">
@@ -12,18 +15,17 @@ const Navbar = () => {
 					<Link to="/" className="brand-logo">
 						Firebase React
 					</Link>
-					<SignedInLinks />
-                    <SignedOutLinks />
+					{auth.uid ? <SignedInLinks /> : <SignedOutLinks />}
 				</div>
 			</nav>
 		</div>
 	);
 };
 
-const mapStateToProps = (state) =>{
-	return ({
-		
-	})
-}
+const mapStateToProps = (state) => {
+	return {
+		auth: state.firebase.auth
+	};
+};
 
 export default connect(mapStateToProps)(Navbar);
