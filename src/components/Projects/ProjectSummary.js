db3c2d42fc6556cab0866a3a9c441ccd960733b4
devWichrowski <wichrowski.patryk92@gmail.com>
2019-04-20 14:26:47 +0200

    Add moment
    31

--- src/components/Projects/ProjectSummary.js
+++ src/components/Projects/ProjectSummary.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
-export default function ProjectSummary({project}) {
+export default function ProjectSummary({ project }) {
 	return (
 		<div className="card z-depth-0 project-summary">
 			<div className="card-content grey-text text-darken-3">
 				<span className="card-title">
 					<p>{project.title}</p>
-					<p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-					<p className="grey-text">{project.content}</p>
+					<p>
+						Posted by {project.authorFirstName} {project.authorLastName}
+					</p>
+					<p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
 				</span>
 			</div>
 		</div>
